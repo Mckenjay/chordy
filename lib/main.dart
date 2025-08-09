7f139f90ce079b5fe6937b5a65c18d418e27685c
Mckenjay <tedbryanrazonado12345@gmail.com>
2025-08-09 18:32:36 +0800

    add AppBarActions widget for enhanced app bar functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chords/appbar_actions.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Chordy',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 71, 58, 183)),
       ),
       home: Scaffold(
         appBar: AppBar(
           actions: [
-            IconButton(
-              tooltip: 'Search',
-              icon: const Icon(Icons.search),
-              onPressed: () {},
-            ),
-            PopupMenuButton<Text>(
-              tooltip: 'More options',
-              itemBuilder: (BuildContext context) {
-                return [
-                  PopupMenuItem(
-                    value: const Text('Settings'),
-                    child: const Text('Settings'),
-                    onTap: () {},
-                  ),
-                  PopupMenuItem(
-                    value: const Text('About'),
-                    child: const Text('About Chordy'),
-                    onTap: () {},
-                  ),
-                ];
-              },
-            ),
+            AppBarActions(),
           ],
           title: const Text("Chordy"),
         ),
