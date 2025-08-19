6a63ca3b69b80e02d941d872f84ef28bb994075e
Mckenjay <tedbryanrazonado12345@gmail.com>
2025-08-19 08:34:28 +0800

    Refactor main application structure: move HomePage to a separate file and update entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_chords/firebase_options.dart';
-import 'package:flutter_chords/appbar_actions.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_chords/pages/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,48 +10,5 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  final songs = 30;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Chordy',
-      theme: ThemeData(
-        textTheme: GoogleFonts.robotoTextTheme(),
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 71, 58, 183)),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          actions: [
-            AppBarActions(),
-          ],
-          title: const Text("Chordy"),
-        ),
-        body: ListView.builder(
-          padding: EdgeInsets.only(left: 10, right: 10),
-          itemCount: songs,
-          itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                leading: Icon(Icons.lyrics_outlined, color: Colors.black),
-                title: Text('Song Lyric ${index+1}', style: TextStyle(fontWeight: FontWeight.bold),),
-                subtitle: Text('Artist'),
-                hoverColor: Colors.grey[300],
-                shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(10)),
-                trailing: Icon(Icons.chevron_right, color: Colors.blue,),
-                onTap: () {},
-              ),
-            );
-          },
-        )
-      ),
-    );
-  }
+  runApp(const HomePage());
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_chords/appbar_actions.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final songs = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chordy',
+      theme: ThemeData(
+        textTheme: GoogleFonts.robotoTextTheme(),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 71, 58, 183)),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            AppBarActions(),
+          ],
+          title: const Text("Chordy"),
+        ),
+        body: ListView.builder(
+          padding: EdgeInsets.only(left: 10, right: 10),
+          itemCount: songs,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                leading: Icon(Icons.lyrics_outlined, color: Colors.black),
+                title: Text('Song Lyric ${index+1}', style: TextStyle(fontWeight: FontWeight.bold),),
+                subtitle: Text('Artist'),
+                hoverColor: Colors.grey[300],
+                shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(10)),
+                trailing: Icon(Icons.chevron_right, color: Colors.blue,),
+                onTap: () {},
+              ),
+            );
+          },
+        )
+      ),
+    );
+  }
+}
