4c2ebb223ee839b2e4c745b8506e76bad950c918
Mckenjay <tedbryanrazonado12345@gmail.com>
2025-08-20 16:31:38 +0800

    Add SongModel and SongService classes for song data management

---
+++ lib/models/song_model.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class SongModel {
+  final String id;
+  final String title;
+  final String artist;
+  final String lyrics;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+
+  SongModel({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.lyrics,
+    this.createdAt,
+    this.updatedAt
+  });
+
+  Map<String, dynamic> toMap() => {
+    'title': title,
+    'titleLower': title.toLowerCase(),
+    'artist': artist,
+    'artistLower': artist.toLowerCase(),
+    'lyrics': lyrics,
+    'createdAt': FieldValue.serverTimestamp(),
+    'updatedAt': FieldValue.serverTimestamp(),
+  };
+
+  Map<String, dynamic> toUpdateMap() => {
+    'title': title,
+    'titleLower': title.toLowerCase(),
+    'artist': artist,
+    'artistLower': artist.toLowerCase(),
+    'lyrics': lyrics,
+    'updatedAt': FieldValue.serverTimestamp(),
+  };
+
+  factory SongModel.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc) {
+    return SongModel(
+      id: doc.id,
+      title: doc.data()!["title"] ?? '',
+      artist: doc.data()!["artist"] ?? '',
+      lyrics: doc.data()!["lyrics"] ?? '',
+    );
+  }
+}
---
+++ lib/services/song_service.dart
@@ -0,0 +1,49 @@
+import 'package:chordy/models/song_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class SongService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<List<SongModel>> getSongs() async {
+    QuerySnapshot<Map<String, dynamic>> snapshot = await _db.collection('Songs').get();
+    return snapshot.docs
+      .map((doc) => SongModel.fromDocumentSnapshot(doc))
+      .toList();
+  }
+
+  Future<void> addSong(SongModel songData) async {
+    await _db.collection('Songs').add(songData.toMap());
+  }
+
+  Future<void> updateSong(SongModel songData) async {
+    await _db.collection('Songs').doc(songData.id).update(songData.toUpdateMap());
+  }
+
+  Future<void> deleteSong(String songID) async {
+    await _db.collection('Songs').doc(songID).delete();
+  }
+
+  Future<List<SongModel>> searchSong(String title) async {
+    QuerySnapshot<Map<String, dynamic>> snapshot = await _db
+      .collection('Songs')
+      .where('titleLower', isGreaterThanOrEqualTo: title.toLowerCase())
+      .where('titleLower', isLessThan: '${title.toLowerCase()}z')
+      .get();
+
+    return snapshot.docs
+      .map((doc) => SongModel.fromDocumentSnapshot(doc))
+      .toList();
+  }
+
+  Future<List<SongModel>> searchArtist(String artist) async {
+    QuerySnapshot<Map<String, dynamic>> snapshot = await _db
+      .collection('Songs')
+      .where('artistLower', isGreaterThanOrEqualTo: artist.toLowerCase())
+      .where('artistLower', isLessThan: '${artist.toLowerCase()}z')
+      .get();
+
+    return snapshot.docs
+      .map((doc) => SongModel.fromDocumentSnapshot(doc))
+      .toList();
+  }
+}
